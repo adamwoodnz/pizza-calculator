982f62877befef1246bbd4ba6f00463c424c5956
Adam Wood <info@adamwood.co.nz>
2018-01-26 11:06:27 +1300

    hoc pattern

--- src/Application.js
+++ src/Application.js
@@ -11,47 +11,45 @@ const initialState = {
   slicesPerPerson: 2,
 };
 
-const WithCalculator = WrappedComponent => {
-  return WrappedComponent;
-};
-
-export default class ApplicationContainer extends Component {
+const WithPizzaCalculations = WrappedComponent => class extends Component {
+  static displayName = `WithPizzaCalculations(${WrappedComponent.displayName || WrappedComponent.name})`;
   state = { ...initialState };
-
+  
   updateNumberOfPeople = event => {
     const numberOfPeople = parseInt(event.target.value, 10);
     this.setState({ numberOfPeople });
   };
-
+  
   updateSlicesPerPerson = event => {
     const slicesPerPerson = parseInt(event.target.value, 10);
     this.setState({ slicesPerPerson });
   };
-
+  
   reset = event => {
     this.setState({ ...initialState });
   };
-
+  
   render() {
     const { numberOfPeople, slicesPerPerson } = this.state;
     const numberOfPizzas = calculatePizzasNeeded(
       numberOfPeople,
       slicesPerPerson,
     );
-
+    
     return (
-      <Application
+      <WrappedComponent
         numberOfPeople={numberOfPeople}
         slicesPerPerson={slicesPerPerson}
         numberOfPizzas={numberOfPizzas}
         updateNumberOfPeople={this.updateNumberOfPeople}
         updateSlicesPerPerson={this.updateSlicesPerPerson}
+        {...this.props}
       />
     );
   }
 }
 
-class Application extends Component {
+class PizzaCalculator extends Component {
   render() {
     const {
       numberOfPeople,
@@ -60,6 +58,7 @@ class Application extends Component {
       updateNumberOfPeople,
       updateSlicesPerPerson,
     } = this.props;
+
     return (
       <div className="Application">
         <Title />
@@ -83,5 +82,13 @@ class Application extends Component {
         </button>
       </div>
     );
+  }
+};
+
+export default class Application extends Component {
+  render() {
+    const PizzaCalculatorWithCalculations = WithPizzaCalculations(PizzaCalculator);
+    
+    return <PizzaCalculatorWithCalculations />;
   }
 }
